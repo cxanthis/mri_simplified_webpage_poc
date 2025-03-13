be69f1d8a590a2110c755ab29e7c053a42828309
cxanthis <cxanthis@gmail.com>
2025-03-13 20:18:21 +0200

    Fix for vercel deployment

--- app/learn-mri/mri-procedures/[content_id]/page.tsx
+++ app/learn-mri/mri-procedures/[content_id]/page.tsx
@@ -1,7 +1,7 @@
 // app/mri-fundamentals/[content_id]/page.tsx
 import { redirect } from 'next/navigation';
 
-export default function ContentPage({ params }: { params: { content_id: string } }) {
+export default async function ContentPage({ params }: { params: { content_id: string } }) {
   // This page will redirect to the main page with a query parameter
   redirect(`/item/${params.content_id}`);
 }
